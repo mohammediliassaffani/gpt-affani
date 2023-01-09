fbf7e987992fedc2df6ae7c6b1a10f49610586d6
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2023-01-09 16:19:29 +0100

    add-assets

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { RiMenu3line, RiCloseLin } from "react-icons/ri";
+import logo from "../../assets/logo.svg";
 import "./navbar.css";
 
 function Navbar() {
   return (
-    <div>
-      <h2>navbar</h2>
+    <div className="gpt__navbar">
+      <div className="gpt__navbar-links">
+        <div className="gpt__navbar-links-logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="gpt__navbar-links-container">
+          <a href="#home"> Home </a>
+          <a href="#wgpt"> What is gpt </a>
+          <a href="#home"> Home </a>
+          <a href="#home"> Home </a>
+        </div>
+      </div>
     </div>
   );
 }
