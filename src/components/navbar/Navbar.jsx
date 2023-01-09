ef784153c76849fffaca8da38c0b95a79aab6f18
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2023-01-09 18:07:16 +0100

    sing-in-button

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -11,10 +11,25 @@ function Navbar() {
           <img src={logo} alt="logo" />
         </div>
         <div className="gpt__navbar-links-container">
-          <a href="#home"> Home </a>
-          <a href="#wgpt"> What is gpt </a>
-          <a href="#home"> Home </a>
-          <a href="#home"> Home </a>
+          <p>
+            <a href="#home"> Home </a>
+          </p>
+          <p>
+            <a href="#wgpt"> What is gpt </a>
+          </p>
+          <p>
+            <a href="#possibility"> OpenIA </a>
+          </p>
+          <p>
+            <a href="#features"> Case Studies </a>
+          </p>
+          <p>
+            <a href="#blog"> Library </a>
+          </p>
+        </div>
+        <div className="gpt__navbar-sing">
+          <p>Sing in</p>
+          <button type="button">Sing Up</button>
         </div>
       </div>
     </div>
