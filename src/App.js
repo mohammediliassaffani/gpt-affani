b2f4648528b90648c4adf4ecaf4ba1b1e573a683
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2023-01-09 13:06:50 +0100

    import-componente

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import Articale from "./components/articale/Articale";
+import Brand from "./components/brand/Brand";
+import Cta from "./components/cta/Cta";
+import Feature from "./components/feature/Feature";
+import Navbar from "./components/navbar/Navbar";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Cta />
+      <Articale />
+      <Feature />
+      <Brand /><
+    </div>
+  );
 }
 
 export default App;
