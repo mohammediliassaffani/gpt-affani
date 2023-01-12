78027a2afefbdf3ca2b4343bdbf2aee2f89da551
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2023-01-12 19:32:36 +0100

    styling-header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,33 @@
 import React from "react";
 import "./header.css";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 
 function Header() {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="gpt__header section__padding" id="home">
+      <div className="gpt__header-content">
+        <h1 className="gradient__text">
+          Let’s Build Something amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <div className="gpt__header-content__input">
+          <input type="email" placeholder="your email adress" />
+          <button type="button"> get started</button>
+        </div>
+        <div className="gpt__header-content_people">
+          <img src={people} alt="people" />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+        <div className="gpt__header-img">
+          <img src={ai} alt="" />
+        </div>
+      </div>
     </div>
   );
 }
