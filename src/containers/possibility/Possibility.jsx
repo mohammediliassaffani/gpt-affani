ad081ed24ff5691f51b171c8695f607185aca537
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2023-01-29 01:06:07 +0100

    add-possibility-img

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import PossibilityImage from "../../assets/possibility.png";
 import "./possibility.css";
 
 const Possibility = () => {
-  return <div className="" id="possibility"></div>;
+  return (
+    <div className="gpt__possibility section__padding" id="possibility">
+      <div className="gpt__possibility-image">
+        <img src={PossibilityImage} alt="possibility" />
+      </div>
+    </div>
+  );
 };
 
 export default Possibility;
