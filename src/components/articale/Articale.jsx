1a4eebba9fb5c05722cadd574b4b95a37822649f
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2023-01-29 01:58:12 +0100

    add-import-blog

--- src/components/articale/Articale.jsx
+++ src/components/articale/Articale.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { Articale } from "../../components";
 import "./articale.css";
 
 function articale() {
   return (
-    <div>
-      <h2>article</h2>
+    <div className="gpt__blog section__padding" id="blog">
+      <div className="gpt__blog-heading">
+        <h1 className="gradient__text">
+          A lot is happening, We are blogging about it.
+        </h1>
+      </div>
+      <div className="gpt__blog-container">
+        <div className="gpt__blog-container_groupA">
+          <Articale />
+        </div>
+      </div>
     </div>
   );
 }
