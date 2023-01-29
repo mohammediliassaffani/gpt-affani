2f4e1b04066d198393fed6fd8b58c95a08d982a7
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2023-01-29 01:35:12 +0100

    finishing-CTA

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./cta.css";
 
 const CTA = () => (
-  <div className="gpt__cta">
+  <div className="gpt__cta" id="CTA">
     <div className="gpt__cta-content">
       <p>Request Early Access to Get Started</p>
       <h3>Register Today & start exploring the endless possibilities.</h3>
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./blog.css";
 
-function Blog() {
+const Blog = () => {
   return (
     <div>
       <h1>Blog</h1>
     </div>
   );
-}
+};
 
 export default Blog;
