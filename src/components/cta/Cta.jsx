eea7973f4c1c54284dc09353478ed9b9dfdb4dee
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2023-01-29 01:32:02 +0100

    add-CTA

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./cta.css";
 
-function Cta() {
-  return (
-    <div>
-      <h2>Cta</h2>
+const CTA = () => (
+  <div className="gpt__cta">
+    <div className="gpt__cta-content">
+      <p>Request Early Access to Get Started</p>
+      <h3>Register Today & start exploring the endless possibilities.</h3>
     </div>
-  );
-}
+    <div className="gpt__cta-btn">
+      <button type="button">Get Started</button>
+    </div>
+  </div>
+);
 
-export default Cta;
+export default CTA;
