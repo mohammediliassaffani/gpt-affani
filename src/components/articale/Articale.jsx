d8a23e235c97e3b978ccab40e64c757019cbd8a4
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2023-01-29 02:13:01 +0100

    adding-img-blog

--- src/components/articale/Articale.jsx
+++ src/components/articale/Articale.jsx
@@ -1,19 +1,11 @@
 import React from "react";
-import { Articale } from "../../components";
 import "./articale.css";
 
-function articale() {
+function articale({ imgUrl }) {
   return (
-    <div className="gpt__blog section__padding" id="blog">
-      <div className="gpt__blog-heading">
-        <h1 className="gradient__text">
-          A lot is happening, We are blogging about it.
-        </h1>
-      </div>
-      <div className="gpt__blog-container">
-        <div className="gpt__blog-container_groupA">
-          <Articale />
-        </div>
+    <div className="gpt__blog-container_articale">
+      <div className="gpt__blog-container_articale-image">
+        <img src={imgUrl} alt="blogPic" />
       </div>
     </div>
   );
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import { Articale } from "../../components";
+import { blog01, blog02, blog03, blog04, blog05 } from "./import";
 import "./blog.css";
 
 const Blog = () => {
   return (
-    <div>
-      <h1>Blog</h1>
+    <div className="gpt__blog section__padding" id="blog">
+      <div className="gpt__blog-heading">
+        <h1 className="gradient__text">
+          A lot is happening, We are blogging about it.
+        </h1>
+      </div>
+      <div className="gpt__blog-container">
+        <div className="gpt__blog-container_groupA">
+          <Articale imgUrl={blog01} />
+        </div>
+        <div className="gpt__blog-container_groupB">
+          <Articale imgUrl={blog02} />
+          <Articale imgUrl={blog03} />
+          <Articale imgUrl={blog04} />
+          <Articale imgUrl={blog05} />
+        </div>
+      </div>
     </div>
   );
 };
